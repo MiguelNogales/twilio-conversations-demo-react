a86c31f7344fa5ce8f1cc35ac9f8dca2ea94b8a8
Miguel Nogales <miguelnogalesb@gmail.com>
2024-11-19 09:09:35 -0400

    Update and rename firebase-config.example to firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA9SHwMdIeHOiCIiNoeSPzFnF_lXZQOrzs",
+  authDomain: "pushmnb-1fcd1.firebaseapp.com",
+  projectId: "pushmnb-1fcd1",
+  storageBucket: "pushmnb-1fcd1.firebasestorage.app",
+  messagingSenderId: "920988668015",
+  appId: "1:920988668015:web:a8bb9e00d882bdf2f6bd9a"
+};
